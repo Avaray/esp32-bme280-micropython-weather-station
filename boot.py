ed29e58bd660390d4b7def221df8ac1420c834d3
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-07 20:37:56 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,9 +1,32 @@
 import sys
-import config # config.py file
-import helpers # helpers.py file
+import machine
+
+try:
+    import config
+except ImportError:
+    print('Error: config.py not found.')
+    sys.exit()
+
+try:
+    import helpers
+except ImportError:
+    print('Error: helpers.py not found.')
+    sys.exit()
 
 print('\nSTARTING\n')
-print('Device name:', config.DEVICE_NAME.upper())
+print('Device name:', config.deviceName.upper())
 print('Platform:', sys.platform.upper())
-print('Micropython Firmware Version', helpers.tuple_to_semver(sys.implementation.version))
-print('Python version', helpers.tuple_to_semver(sys.version_info))
+print('Micropython Firmware Version', helpers.tupleToSemver(sys.implementation.version))
+print('Python version', helpers.tupleToSemver(sys.version_info))
+print('CPU frequency:', machine.freq(), 'Hz')
+
+if not config.knownNetworks:
+    print('No known networks found in config.py')
+    print('Please add at least one network to knownNetworks list')
+    sys.exit()
+else:
+    print('Known networks:', len(config.knownNetworks))
+
+# Set CPU frequency to 80MHz (default is 160MHz) to save power
+print('Setting CPU frequency to 80MHz')
+machine.freq(80000000)
