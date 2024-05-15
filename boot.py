141381a72436046d84f7bef49690d4b4d386fab6
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-15 09:22:15 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -4,16 +4,19 @@ import machine
 import time
 
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
-    print('Woke from a deep sleep')
+  print('Woke from a deep sleep')
 else:
-    print('Booting after a hard reset or power on')
-    print('Press Ctrl-C to interrupt boot and start a REPL session\n')
-    # wait 3 seconds for user to interrupt the boot process 
-    try:
-        for i in range(3, 0, -1):
-            print(i, end='...')
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print('\nInterrupted by keyboard (Ctrl-C)\n')
-    else:
-        print('0')
+  print('Booting after a hard reset or power on')
+
+print('Press Ctrl-C to interrupt boot and start a REPL session\n')
+# wait 5 seconds for user to interrupt the boot process 
+try:
+  for i in range(5, 0, -1):
+    print(i, end='...')
+    time.sleep(1)
+except KeyboardInterrupt:
+  print('\nInterrupted by keyboard (Ctrl-C)\n')
+else:
+  print('0')
+
+import index
