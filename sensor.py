796fdb25035f5829a0cc463d8124edaa8fe083cf
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-17 22:00:18 +0200

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,9 +1,12 @@
+# official micropython modules
 from machine import SoftI2C, Pin
 import BME280
 
+# modules from the project
+import config
 import helpers
 
-i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
+i2c = SoftI2C(scl=Pin(config.BME280_SCL_PIN), sda=Pin(config.BME280_SDA_PIN))
 sensor = BME280.BME280(i2c)
 
 def isValid(value):
