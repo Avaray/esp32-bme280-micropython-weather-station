2e1d04f9c19668c135d5d5317e4cb15709961566
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-18 10:55:41 +0200

    added sample config

---
+++ config.py
@@ -0,0 +1,23 @@
+# Name for your device (optional)
+deviceId = ""
+
+# List of Wi-Fi networks
+# You can specify multiple networks. The device will try to connect to them in order
+networks = [
+  {"ssid": "", "password": ""}
+]
+
+# List of servers (URL's to upload sensor readings; names are optional)
+# You Need to specify protocol (http/https) in the URL
+# Without specified protocol the request might not be sent
+servers = [
+  {"name": "", "url": ""}
+]
+
+# SCL and SDA pins for BME280 sensor
+BME280_SCL_PIN = 22
+BME280_SDA_PIN = 21
+
+# How often check readings and send them to server (in minutes)
+# In other words, how often the device will wake up (from deep sleep) and send readings
+frequency = 15
