df689b1e6abe6361aa8779b39b4cb8f04d767806
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-19 11:16:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # Name for your device (optional)
 deviceId = ""
 
-# List of Wi-Fi networks
+# List of known Wi-Fi networks
 # You can specify multiple networks. The device will try to connect to them in order
+# It will not connect to networks that are not on this list
 networks = [
   {"ssid": "", "password": ""}
 ]
@@ -22,3 +23,15 @@ BME280_SDA_PIN = 21
 # How often check readings and send them to server (in minutes)
 # In other words, how often the device will wake up (from deep sleep) and send readings
 frequency = 15
+
+# Enable or disable logs
+LOGS_ENABLED = True
+
+# Save logs on device
+LOGS_SAVE = True
+
+# Directory for log files (optional; default: logs)
+LOGS_DIR = "logs"
+
+# How many log files to keep on the device (optional; default: 20)
+LOGS_MAX_FILES = 20
