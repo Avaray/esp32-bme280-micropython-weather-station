d0206ace147581b7f278ddb7c93ecbb350bd9edd
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-20 19:35:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ SERVERS = [
   {"name": "", "url": ""}
 ]
 
-# SCL and SDA pins for BME280 sensor
-BME280_SCL_PIN = 22
-BME280_SDA_PIN = 21
+# SCL and SDA pins for sensor
+SENSOR_SCL_PIN = 22
+SENSOR_SDA_PIN = 21
 
 # How often check readings and send them to server (in minutes)
 # In other words, how often the device will wake up (from deep sleep) and send readings
@@ -36,6 +36,18 @@ LOGS_DIR = "logs"
 # How many log files to keep on the device (optional; default: 20)
 LOGS_MAX_FILES = 20
 
-# Enable or disable OTA updates (disabled by default)
+# Enable or disable Over-The-Air updates 
 # If enabled, the device will check for updates on start and download them if available
-UPDATE_ON_START = False
+OTA_UPDATES = False
+
+# By default the device will boot in Normal Mode when ADMIN_MODE_PIN_OUT and ADMIN_MODE_PIN_IN are connected (shorted)
+# If you want to reverse this behavior, set ADMIN_MODE_REVERSED to True
+ADMIN_MODE_REVERSED = False
+
+# GPIO pins for Admin Mode
+ADMIN_MODE_PIN_OUT = 17
+ADMIN_MODE_PIN_IN = 16
+
+# Wi-Fi SSID and password for Admin Mode (optional)
+ADMIN_MODE_WIFI_SSID = ""
+ADMIN_MODE_WIFI_PASSWORD = ""
