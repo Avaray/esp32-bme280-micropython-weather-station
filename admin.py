1c8f3d6ef475019f579e182eed3a46bbb39b4516
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-25 01:33:26 +0200

    fixed AP

--- admin.py
+++ admin.py
@@ -12,10 +12,9 @@ ap = network.WLAN(network.AP_IF)
 ap.active(True)
 
 # Set the AP configuration
-ssid = config.AP_SSID if isinstance(config.AP_SSID, str) and config.AP_SSID else 'Weather Station'
-password = config.AP_PASSWORD if isinstance(config.AP_PASSWORD, str) and config.AP_PASSWORD else None
+ssid = config.AP_SSID if isinstance(config.ADMIN_MODE_WIFI_SSID, str) and config.ADMIN_MODE_WIFI_SSID else 'Weather Station'
+password = str(config.ADMIN_MODE_WIFI_PASSWORD) if isinstance(config.ADMIN_MODE_WIFI_PASSWORD, str) and config.ADMIN_MODE_WIFI_PASSWORD else ''
 authmode = network.AUTH_WPA_WPA2_PSK if password else network.AUTH_OPEN
 ap.config(ssid=ssid, authmode=authmode, password=password)
 
 print('AP started:', ap.ifconfig())
-
