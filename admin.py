ef6e3dc91da859cb093947174a11ad7f7b5ff25f
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-28 15:46:27 +0200

    formatted

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
-# Issues: 
+# Issues:
 # Captive Portal is not opening on Samsung phone (A51). Does Samsung require SSL?
 
-print('\nRunning Admin Mode code. This code is not finished yet.\n')
+print("\nRunning Admin Mode code. This code is not finished yet.\n")
 
 import gc
 import network
@@ -13,10 +13,16 @@ import ustruct as struct
 import config
 
 # Access Point settings
-SERVER_SSID = config.ADMIN_MODE_WIFI_SSID if config.ADMIN_MODE_WIFI_SSID else 'Weather Station'
-SERVER_AUTHMODE = network.AUTH_OPEN if not config.ADMIN_MODE_WIFI_PASSWORD else network.AUTH_WPA_WPA2_PSK
-SERVER_IP = '10.0.0.1'
-SERVER_SUBNET = '255.255.255.0'
+SERVER_SSID = (
+    config.ADMIN_MODE_WIFI_SSID if config.ADMIN_MODE_WIFI_SSID else "Weather Station"
+)
+SERVER_AUTHMODE = (
+    network.AUTH_OPEN
+    if not config.ADMIN_MODE_WIFI_PASSWORD
+    else network.AUTH_WPA_WPA2_PSK
+)
+SERVER_IP = "10.0.0.1"
+SERVER_SUBNET = "255.255.255.0"
 
 
 def wifi_start_access_point():
@@ -24,12 +30,12 @@ def wifi_start_access_point():
     wifi.active(True)
     wifi.ifconfig((SERVER_IP, SERVER_SUBNET, SERVER_IP, SERVER_IP))
     wifi.config(essid=SERVER_SSID, authmode=SERVER_AUTHMODE)
-    print('Network config:', wifi.ifconfig())
+    print("Network config:", wifi.ifconfig())
 
 
 def _handle_exception(loop, context):
-    """ uasyncio v3 only: global exception handler """
-    print('Global exception handler')
+    """uasyncio v3 only: global exception handler"""
+    print("Global exception handler")
     sys.print_exception(context["exception"])
     sys.exit()
 
@@ -37,13 +43,13 @@ def _handle_exception(loop, context):
 class DNSQuery:
     def __init__(self, data):
         self.data = data
-        self.domain = ''
+        self.domain = ""
         tipo = (data[2] >> 3) & 15  # Opcode bits
         if tipo == 0:  # Standard query
             ini = 12
             lon = data[ini]
             while lon != 0:
-                self.domain += data[ini + 1:ini + lon + 1].decode('utf-8') + '.'
+                self.domain += data[ini + 1 : ini + lon + 1].decode("utf-8") + "."
                 ini += lon + 1
                 lon = data[ini]
         print("DNSQuery domain:" + self.domain)
@@ -51,19 +57,23 @@ class DNSQuery:
     def response(self, ip):
         print("DNSQuery response: {} ==> {}".format(self.domain, ip))
         if self.domain:
-            packet = self.data[:2] + b'\x81\x80'
-            packet += self.data[4:6] + self.data[4:6] + b'\x00\x00\x00\x00'  # Questions and Answers Counts
+            packet = self.data[:2] + b"\x81\x80"
+            packet += (
+                self.data[4:6] + self.data[4:6] + b"\x00\x00\x00\x00"
+            )  # Questions and Answers Counts
             packet += self.data[12:]  # Original Domain Name Question
-            packet += b'\xC0\x0C'  # Pointer to domain name
-            packet += b'\x00\x01\x00\x01\x00\x00\x00\x3C\x00\x04'  # Response type, ttl and resource data length -> 4 bytes
-            packet += struct.pack('!4B', *[int(x) for x in ip.split('.')])  # 4 bytes of IP
+            packet += b"\xc0\x0c"  # Pointer to domain name
+            packet += b"\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04"  # Response type, ttl and resource data length -> 4 bytes
+            packet += struct.pack(
+                "!4B", *[int(x) for x in ip.split(".")]
+            )  # 4 bytes of IP
         return packet
 
 
 def get_file_content(file_path):
-    """ Function to read a file from the /web directory """
+    """Function to read a file from the /web directory"""
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             return file.read()
     except OSError:
         return None
@@ -87,7 +97,7 @@ class MyApp:
         loop.create_task(self.run_dns_server())
 
         # Start looping forever
-        print('Looping forever...')
+        print("Looping forever...")
         await loop.run_forever()
 
     async def handle_http_connection(self, reader, writer):
@@ -96,60 +106,62 @@ class MyApp:
         # Get HTTP request line
         data = await reader.readline()
         request_line = data.decode()
-        addr = writer.get_extra_info('peername')
-        print('Received {} from {}'.format(request_line.strip(), addr))
+        addr = writer.get_extra_info("peername")
+        print("Received {} from {}".format(request_line.strip(), addr))
 
         # Read headers
         while True:
             gc.collect()
             line = await reader.readline()
-            if line == b'\r\n':
+            if line == b"\r\n":
                 break
 
         # Handle the request
         if len(request_line) > 0:
             try:
-                path = request_line.split(' ')[1]
+                path = request_line.split(" ")[1]
             except IndexError:
-                path = '/'
+                path = "/"
 
             # https://github.com/NickSto/uptest/blob/master/captive-portals.md
             # https://github.com/tripflex/captive-portal?tab=readme-ov-file#known-endpoints
             # https://en.wikipedia.org/wiki/Captive_portal
             captive_portal_urls = [
-                '/',
-                '/check_network_status.txt',
-                '/connecttest.txt',
-                '/gen_204',
-                '/generate_204', 
-                '/hotspot-detect.html',
-                '/hotspot-detect.html', 
-                '/hotspotdetect.html',
-                '/kindle-wifi/wifiredirect.html',
-                '/library/test/success.html',
-                '/mobile/status.php',
-                '/ncsi.txt', 
-                '/success.html', 
-                '/success.txt',
+                "/",
+                "/check_network_status.txt",
+                "/connecttest.txt",
+                "/gen_204",
+                "/generate_204",
+                "/hotspot-detect.html",
+                "/hotspot-detect.html",
+                "/hotspotdetect.html",
+                "/kindle-wifi/wifiredirect.html",
+                "/library/test/success.html",
+                "/mobile/status.php",
+                "/ncsi.txt",
+                "/success.html",
+                "/success.txt",
             ]
 
             # TODO
             # I'm thinking about redirect all requests to the captive portal, except the ones that are for the web server files
             # List of files is available to get using utils.getProjectFilesList() function
             # files = utils.getProjectFilesList().['web']
-                    
+
             # Serve captive portal redirect
             if path in captive_portal_urls:
-                response = "HTTP/1.0 302 Found\r\nLocation: http://10.0.0.1/index.html\r\n\r\n"
+                response = (
+                    "HTTP/1.0 302 Found\r\nLocation: http://10.0.0.1/index.html\r\n\r\n"
+                )
                 await writer.awrite(response)
             else:
                 # Serve static files
-                if path == '/':
-                    path = '/index.html'  # Default file to serve
+                if path == "/":
+                    path = "/index.html"  # Default file to serve
 
-                file_path = '/web' + path
+                file_path = "/web" + path
                 try:
-                    with open(file_path, 'r') as file:
+                    with open(file_path, "r") as file:
                         await writer.awrite("HTTP/1.0 200 OK\r\n\r\n")
                         while True:
                             chunk = file.read(1024)
@@ -157,11 +169,16 @@ class MyApp:
                                 break
                             await writer.awrite(chunk)
                 except OSError:
-                    missing_content = get_file_content('/web/404.html')
+                    missing_content = get_file_content("/web/404.html")
                     if missing_content:
-                        response = "HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n" + missing_content
+                        response = (
+                            "HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+                            + missing_content
+                        )
                     else:
-                        response = "HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+                        response = (
+                            "HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+                        )
                         response += "<html><head><title>404 Not Found</title><body>File not found</body></html>"
                     await writer.awrite(response)
 
@@ -172,7 +189,7 @@ class MyApp:
     async def run_dns_server(self):
         udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         udps.setblocking(False)
-        udps.bind(('0.0.0.0', 53))
+        udps.bind(("0.0.0.0", 53))
 
         while True:
             try:
@@ -200,6 +217,6 @@ try:
     myapp = MyApp()
     asyncio.run(myapp.start())
 except KeyboardInterrupt:
-    print('Interrupted with keyboard')
+    print("Interrupted with keyboard")
 finally:
     asyncio.new_event_loop()  # Clear retained state
