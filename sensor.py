0a4a6669f33b82c494236881037039821fd6f7b0
Avaray <1459553+Avaray@users.noreply.github.com>
2024-05-30 08:56:31 +0200

    moving to snake case, changed imports

--- sensor.py
+++ sensor.py
@@ -2,12 +2,12 @@
 from machine import SoftI2C, Pin
 
 # Modules from the project
-import config
-import utils
+from config import SENSOR_SCL_PIN, SENSOR_SDA_PIN
+from utils import normalize_number
 import BME280
 
 # Initialize I2C bus
-i2c = SoftI2C(scl=Pin(config.SENSOR_SCL_PIN), sda=Pin(config.SENSOR_SDA_PIN))
+i2c = SoftI2C(scl=Pin(SENSOR_SCL_PIN), sda=Pin(SENSOR_SDA_PIN))
 
 # Initialize BME280 sensor
 sensor = BME280.BME280(i2c)
@@ -20,9 +20,9 @@ def read():
   sensor.read()
   data = {}
   if isValid(sensor.temperature):
-    data['temperature'] = utils.normalizeNumber(sensor.temperature)
+    data['temperature'] = normalize_number(sensor.temperature)
   if isValid(sensor.pressure):
-    data['pressure'] = utils.normalizeNumber(sensor.pressure)
+    data['pressure'] = normalize_number(sensor.pressure)
   if isValid(sensor.humidity):
-    data['humidity'] = utils.normalizeNumber(sensor.humidity)
+    data['humidity'] = normalize_number(sensor.humidity)
   return data
